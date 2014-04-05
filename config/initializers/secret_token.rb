eb290bd2780a2b90fe86fc62c60e608d3cc4c41a
Philip Hale <phil989@gmail.com>
2014-04-06 00:18:51 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NlpSampleApp::Application.config.secret_key_base = '3e9adc9f7a9173509e54fbe27a159f86e24528fcf8bbf797c5a7507972a273806cc945b672181cd547f8aac2dc0ad0a867601bbd417a074e99557c2473c0728b'
