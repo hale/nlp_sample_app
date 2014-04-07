7f472b893c25b7fe9fbfac2a72653cb086fcbcc1
Philip Hale <phil989@gmail.com>
2014-04-07 18:29:10 +0100

    Add a search results page search#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ NlpSampleApp::Application.routes.draw do
 
   root 'home#index'
 
+  resources :search, only: [:index]
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe SearchController do
 
+  describe "#index" do
+    it "works" do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it "assigns some results" do
+      get :index
+      expect(assigns(:results)).not_to be_empty
+    end
+
+  end
+
 end
