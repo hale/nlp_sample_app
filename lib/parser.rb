34f7c51b1eab2d0c86bf63f1b43906d4a8010fb5
Philip Hale <phil989@gmail.com>
2014-04-09 02:06:07 +0100

    Convert parser tests to spec.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,43 +1,40 @@
 require 'sanitize'
 
-module Parser
-  class Parser
+class Parser
+  ROOT = File.expand_path(File.dirname(__FILE__))
+  SCI_FI_DIR = ROOT + "/assets/gutenberg_sci_fi"
 
-    ROOT = File.expand_path(File.dirname(__FILE__))
-    SCI_FI_DIR = ROOT + "/assets/gutenberg_sci_fi"
+  Book = Struct.new(:content, :title)
 
-    Book = Struct.new(:content, :title)
-
-    def self.get_books
-      books = []
-      get_filenames.each do |filename|
-        print "Adding #{filename}..."
-        begin
-          books << parse_book(filename)
-          puts "SUCCESS"
-        rescue ArgumentError
-          puts "ERROR"
-        end
+  def self.get_books
+    books = []
+    get_filenames.each do |filename|
+      print "Adding #{filename}..."
+      begin
+        books << parse_book(filename)
+        puts "SUCCESS"
+      rescue ArgumentError
+        puts "ERROR"
       end
-      books
     end
+    books
+  end
 
-    def self.parse_book(filename)
-      content = IO.read("#{SCI_FI_DIR}/#{filename}")
+  def self.parse_book(filename)
+    content = IO.read("#{SCI_FI_DIR}/#{filename}")
 
-      title = nil
-      content.split(/\n/).each do |line|
-        title ||= line[7..-1].chomp if line[0..6] == "Title: "
+    title = nil
+    content.split(/\n/).each do |line|
+      title ||= line[7..-1].chomp if line[0..6] == "Title: "
+    end
+    Book.new(content, title)
+  end
+
+  def self.get_filenames
+    [].tap do |list|
+      Dir.new(SCI_FI_DIR).each do |filename|
+        list << filename
       end
-      Book.new(content, title)
-    end
-
-    def self.get_filenames
-      [].tap do |list|
-        Dir.new(SCI_FI_DIR).each do |filename|
-          list << filename
-        end
-      end - [".", "..", ".DS_Store"]
-    end
+    end - [".", "..", ".DS_Store"]
   end
 end
---
+++ spec/lib/parser_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Parser do
+  it "extracts the title" do
+    filename = "19362.txt"
+    title = "In the Year 2889"
+    book = Parser.parse_book(filename)
+
+    expect(book.title).to eq(title)
+  end
+
+  it "extracts the content" do
+    filename = "19362.txt"
+    first_line = "Project Gutenberg's In the Year 2889, by Jules Verne and Michel Verne"
+    book = Parser.parse_book(filename)
+
+    expect(book.content.split(/\n/, 2).first.chomp).to eq(first_line)
+  end
+
+  it "gets the right filenames" do
+    expect(Parser.get_filenames.size).to eq(151)
+  end
+end
